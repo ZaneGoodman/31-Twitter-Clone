eae38106edd1fe502fef022b9ebfd4d1038d0f16
Zane_Goodman <zgoodman1776@gmail.com>
2023-07-27 12:11:22 -0400

    Continued Testing

--- test_message_views.py
+++ test_message_views.py
@@ -21,7 +21,7 @@ os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
 # Now we can import app
 
 from app import app, CURR_USER_KEY
-
+from sqlalchemy.orm.exc import DetachedInstanceError
 # Create our tables (we do this here, so we only create the tables
 # once for all tests --- in each test, we'll delete the data
 # and create fresh new clean test data
@@ -48,11 +48,14 @@ class MessageViewTestCase(TestCase):
                                     email="test@test.com",
                                     password="testuser",
                                     image_url=None)
-
+       
         db.session.commit()
 
+    def tearDown(self):
+        db.session.rollback()
+
     def test_add_message(self):
-        """Can use add a message?"""
+        """Can user add a message?"""
 
         # Since we need to change the session to mimic logging in,
         # we need to use the changing-session trick:
@@ -71,3 +74,87 @@ class MessageViewTestCase(TestCase):
 
             msg = Message.query.one()
             self.assertEqual(msg.text, "Hello")
+        
+    def test_delete_message(self):
+        """Can a logged in User delete a message?"""
+
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.testuser.id
+
+
+        test_message = Message(text="Goodbye")
+        user = User.query.get(sess[CURR_USER_KEY])
+        user.messages.append(test_message)
+   
+        db.session.commit()
+        
+
+        response = c.post(f"/messages/{test_message.id}/delete")
+
+        self.assertEqual(response.status_code, 302)
+        
+        self.assertEqual(len(User.query.get(sess[CURR_USER_KEY]).messages), 0)
+
+    def test_logged_out_new_message(self):
+        """When youre logged out, are you prohibited from adding messages?"""
+
+        with self.client as c:
+
+            resp = c.post("/messages/new", follow_redirects=True, data={"text": "Hello"})
+            html = resp.get_data(as_text=True)
+            # Make sure it redirects
+           
+            self.assertIn('<div class="alert alert-danger">Access unauthorized.</div>', html)
+            self.assertEqual(resp.status_code, 200)
+
+    def test_logged_out_delete_message(self):
+        """When youre logged out, are you prohibited from deleting messages?"""
+        with self.client as c:
+            
+            test_message = Message(text="Goodbye")
+            self.testuser.messages.append(test_message)
+   
+            db.session.commit()
+        
+
+            response = c.post(f"/messages/{test_message.id}/delete")
+
+            self.assertEqual(response.status_code, 302)
+            self.assertEqual(len(self.testuser.messages), 1)
+            msg = Message.query.one()
+            self.assertEqual(msg.text, "Goodbye")
+
+    def test_add_message_as_other_user(self):
+        """When youre logged in, are you prohibiting from adding a message as another user?"""
+
+
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = 123456
+
+
+        resp = c.post("/messages/new", follow_redirects=True,data={"text": "Hello"}) 
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertIn("Access unauthorized.", str(resp.data))
+
+    def test_add_message_as_other_user(self):
+        """When youre logged in, are you prohibiting from deleting a message as another user?"""
+
+
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = 123456
+
+        test_message = Message(text="Goodbye")
+        db.session.add(test_message)
+        db.session.commit()
+
+        resp = c.post(f"/messages/{test_message.id}/delete", follow_redirects=True) 
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertIn("Access unauthorized.", str(resp.data))
+
+
+
